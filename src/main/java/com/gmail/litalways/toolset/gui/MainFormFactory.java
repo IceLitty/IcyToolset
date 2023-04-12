63accbef198d47831bf6b9cf721d383473c1be15
IceLitty <yzw9410@qq.com>
2023-04-12 16:25:23 +0800

    Merge message bundle

--- src/main/java/com/gmail/litalways/toolset/gui/MainFormFactory.java
+++ src/main/java/com/gmail/litalways/toolset/gui/MainFormFactory.java
@@ -1,5 +1,6 @@
 package com.gmail.litalways.toolset.gui;
 
+import com.gmail.litalways.toolset.util.MessageUtil;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
@@ -15,8 +16,6 @@ import java.util.ResourceBundle;
  */
 public class MainFormFactory implements ToolWindowFactory {
 
-    private static final ResourceBundle messageBundle = ResourceBundle.getBundle("formTitle");
-
     @Override
     @SuppressWarnings("unused")
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
@@ -24,14 +23,14 @@ public class MainFormFactory implements ToolWindowFactory {
         //
         ToolWindowConvert toolWindowConvert = new ToolWindowConvert(project, toolWindow);
         // createContent(component, displayName, isLockable) https://dploeger.github.io/intellij-api-doc/com/intellij/ui/content/ContentFactory.html
-        Content toolWindowConvertContent = contentFactory.createContent(toolWindowConvert.getContent(), messageBundle.getString("convert.tab.title"), true);
+        Content toolWindowConvertContent = contentFactory.createContent(toolWindowConvert.getContent(), MessageUtil.getMessage("convert.tab.title"), true);
         toolWindow.getContentManager().addContent(toolWindowConvertContent);
         MainFormConvertCommonFunction convertCommon = new MainFormConvertCommonFunction(toolWindowConvert);
         MainFormConvertImgBase64Function convertImgBase64 = new MainFormConvertImgBase64Function(toolWindowConvert);
         MainFormConvertSplitterFunction convertSplitter = new MainFormConvertSplitterFunction(toolWindowConvert);
         //
         ToolWindowEncrypt toolWindowEncrypt = new ToolWindowEncrypt(project, toolWindow);
-        Content toolWindowEncryptContent = contentFactory.createContent(toolWindowEncrypt.getContent(), messageBundle.getString("encrypt.tab.title"), false);
+        Content toolWindowEncryptContent = contentFactory.createContent(toolWindowEncrypt.getContent(), MessageUtil.getMessage("encrypt.tab.title"), false);
         toolWindow.getContentManager().addContent(toolWindowEncryptContent);
         MainFormEncryptHashFunction encryptHash = new MainFormEncryptHashFunction(toolWindowEncrypt);
         MainFormEncryptAsymmetricFunction encryptAsymmetric = new MainFormEncryptAsymmetricFunction(toolWindowEncrypt);
@@ -39,26 +38,26 @@ public class MainFormFactory implements ToolWindowFactory {
         toolWindowEncryptContent.setCloseable(true);
         //
         ToolWindowQRCode toolWindowQRCode = new ToolWindowQRCode(project, toolWindow);
-        Content toolWindowQRCodeContent = contentFactory.createContent(toolWindowQRCode.getContent(), messageBundle.getString("qr.tab.title"), false);
+        Content toolWindowQRCodeContent = contentFactory.createContent(toolWindowQRCode.getContent(), MessageUtil.getMessage("qr.tab.title"), false);
         toolWindow.getContentManager().addContent(toolWindowQRCodeContent);
         MainFormZxingFunction zxing = new MainFormZxingFunction(toolWindowQRCode);
         //
         ToolWindowFormat toolWindowFormat = new ToolWindowFormat(project, toolWindow);
-        Content toolWindowFormatContent = contentFactory.createContent(toolWindowFormat.getContent(), messageBundle.getString("format.tab.title"), false);
+        Content toolWindowFormatContent = contentFactory.createContent(toolWindowFormat.getContent(), MessageUtil.getMessage("format.tab.title"), false);
         toolWindow.getContentManager().addContent(toolWindowFormatContent);
         MainFormFormatFunction format = new MainFormFormatFunction(toolWindowFormat);
         //
         ToolWindowScript toolWindowScript = new ToolWindowScript(project, toolWindow);
-        Content toolWindowScriptContent = contentFactory.createContent(toolWindowScript.getContent(), messageBundle.getString("script.tab.title"), false);
+        Content toolWindowScriptContent = contentFactory.createContent(toolWindowScript.getContent(), MessageUtil.getMessage("script.tab.title"), false);
         toolWindow.getContentManager().addContent(toolWindowScriptContent);
         MainFormScriptFunction script = new MainFormScriptFunction(toolWindowScript);
         //
         ToolWindowSymbol toolWindowSymbol = new ToolWindowSymbol(project, toolWindow);
-        Content toolWindowSymbolContent = contentFactory.createContent(toolWindowSymbol.getContent(), messageBundle.getString("symbol.tab.title"), false);
+        Content toolWindowSymbolContent = contentFactory.createContent(toolWindowSymbol.getContent(), MessageUtil.getMessage("symbol.tab.title"), false);
         toolWindow.getContentManager().addContent(toolWindowSymbolContent);
         //
         ToolWindowOther toolWindowOther = new ToolWindowOther(project, toolWindow);
-        Content toolWindowOtherContent = contentFactory.createContent(toolWindowOther.getContent(), messageBundle.getString("other.tab.title"), false);
+        Content toolWindowOtherContent = contentFactory.createContent(toolWindowOther.getContent(), MessageUtil.getMessage("other.tab.title"), false);
         toolWindow.getContentManager().addContent(toolWindowOtherContent);
         MainFormAboutFunction about = new MainFormAboutFunction(toolWindowOther);
     }
