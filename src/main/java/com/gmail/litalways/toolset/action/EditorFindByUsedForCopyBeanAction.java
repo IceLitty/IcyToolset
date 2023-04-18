9719082f8100a3834ec5c0d6839c69ff4329eb74
IceLitty <yzw9410@qq.com>
2023-04-18 17:44:26 +0800

    CopyBean find usage in Project view popup menu

--- src/main/java/com/gmail/litalways/toolset/action/EditorFindByUsedForCopyBeanAction.java
+++ src/main/java/com/gmail/litalways/toolset/action/EditorFindByUsedForCopyBeanAction.java
@@ -1,17 +1,31 @@
 package com.gmail.litalways.toolset.action;
 
+import com.intellij.codeInsight.generation.PsiMethodMember;
 import com.intellij.codeInsight.intention.IntentionAction;
 import com.intellij.codeInsight.intention.preview.IntentionPreviewInfo;
 import com.intellij.codeInspection.util.IntentionFamilyName;
 import com.intellij.codeInspection.util.IntentionName;
+import com.intellij.find.FindManager;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
-import com.intellij.psi.PsiFile;
+import com.intellij.psi.*;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.search.PsiShortNamesCache;
+import com.intellij.psi.search.searches.MethodReferencesSearch;
+import com.intellij.psi.search.searches.ReferencesSearch;
 import com.intellij.util.IncorrectOperationException;
+import com.intellij.util.Query;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
 /**
  * 在Editor控件显示编辑提示，用以寻找该类/实例是否被各类BeanUtil进行拷贝
+ * TODO 这个还是原型，ProjectView那个已经实现好了，这个考虑改成不搜索实例只搜索类型？还是识别选中的identifier的类型然后分别处理？
  *
  * @author IceRain
  * @since 2023/04/17
@@ -48,6 +62,70 @@ public class EditorFindByUsedForCopyBeanAction implements IntentionAction {
         System.out.println("Project " + project); // Project(name=untitled1, containerState=COMPONENT_CREATED, componentStore=C:\Users\IceRain\IdeaProjects\ untitled1)
         System.out.println("Editor " + editor.getClass().getName() + " " + editor); // {} or EditorImpl[file://C:/Users/IceRain/IdeaProjects/untitled1/src/Main.java]
         System.out.println("Psi " + psiFile); // PsiJavaFile:Main.java
+        int offset = editor.getCaretModel().getOffset();
+        PsiElement psiElement = psiFile.findElementAt(offset);
+        if (psiElement == null) {
+            return;
+        }
+//        FindManager.getInstance(project).findUsages(psiElement, true);
+        PsiType psiType = ((PsiLocalVariable) psiElement.getParent()).getType();
+        PsiClass psiClass = ((PsiClassType) psiType).resolve();
+        PsiMethodMember psiMethodMember = null;
+        for (PsiMethod psiMethod : psiClass.getAllMethods()) {
+            if (psiMethod.getName().startsWith("set")) {
+                psiMethodMember = new PsiMethodMember(psiMethod);
+                break;
+            }
+        }
+        if (psiMethodMember == null) {
+            return;
+        }
+//        Query<PsiReference> psiReferences = MethodReferencesSearch.search(psiMethodMember.getElement(), true);
+//        Collection<PsiReference> psiReferencesAll = psiReferences.findAll();
+//        System.out.println(psiReferencesAll);
+        PsiShortNamesCache psiShortNamesCache = PsiShortNamesCache.getInstance(project);
+        @NotNull String[] allClassNames = psiShortNamesCache.getAllClassNames();
+        List<PsiClass> psiClasses = new ArrayList<>();
+        for (String className : allClassNames) {
+            if (className.contains("BeanUtil") || className.contains("TypeUtil")) {
+                @NotNull PsiClass[] classesByName = psiShortNamesCache.getClassesByName(className, GlobalSearchScope.allScope(project));
+                psiClasses.addAll(Arrays.stream(classesByName).toList());
+            }
+        }
+        System.out.println("PsiClass:");
+        for (PsiClass psiClass1 : psiClasses) {
+            System.out.println(psiClass1.getQualifiedName());
+            Query<PsiReference> classReference = ReferencesSearch.search(psiClass1);
+            Collection<PsiReference> classReferenceAll = classReference.findAll();
+            for (PsiReference pr : classReferenceAll) {
+                if (pr instanceof PsiReferenceExpression pre) {
+                    PsiElement _pre = pre;
+                    PsiMethodCallExpression pmce = null;
+                    for (;;) {
+                        if (_pre == null) {
+                            break;
+                        } if (_pre instanceof PsiMethodCallExpression _pmce) {
+                            pmce = _pmce;
+                            break;
+                        } else {
+                            _pre = _pre.getParent();
+                        }
+                    }
+                    if (pmce != null) {
+                        PsiExpression[] psiExpressions = pmce.getArgumentList().getExpressions();
+                        for (PsiExpression pe : psiExpressions) {
+                            if (pe.getType() instanceof PsiClassType pct) {
+                                PsiClass searchedClass = pct.resolve();
+                                if (searchedClass == psiClass) {
+                                    // 查询到了被BeanUtil调用的该类型
+                                }
+                            }
+                        }
+                    }
+                }
+                System.out.println(" -> " + pr.getCanonicalText());
+            }
+        }
     }
 
     @Override
--- src/main/java/com/gmail/litalways/toolset/action/ProjectViewFindByUsedForCopyBeanAction.java
+++ src/main/java/com/gmail/litalways/toolset/action/ProjectViewFindByUsedForCopyBeanAction.java
@@ -4,19 +4,94 @@ import com.intellij.openapi.actionSystem.ActionUpdateThread;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.LangDataKeys;
-import com.intellij.psi.PsiFile;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.*;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.search.PsiShortNamesCache;
+import com.intellij.psi.search.searches.ReferencesSearch;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.usageView.UsageInfo;
+import com.intellij.usages.*;
+import com.intellij.util.Query;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
 /**
+ * 寻找该右键出来文件的类是否被各种BeanUtil操作过，并显示在Find Usage窗口
+ *
  * @author IceRain
  * @since 2023/04/14
  */
-@SuppressWarnings("ComponentNotRegistered")
 public class ProjectViewFindByUsedForCopyBeanAction extends AnAction {
 
     @Override
     public void actionPerformed(AnActionEvent e) {
-        // TODO: 寻找该右键出来文件的实例是否被各种BeanUtil操作过，并显示find usages窗口
+        Project project = e.getProject();
+        PsiFile file = e.getData(LangDataKeys.PSI_FILE);
+        if (project == null || file == null) {
+            return;
+        }
+        // 获取选择的类
+        PsiClass psiClass = PsiTreeUtil.findChildOfAnyType(file, PsiClass.class);
+        // 获取全部疑似BeanUtil的类
+        PsiShortNamesCache allClassCache = PsiShortNamesCache.getInstance(project);
+        @NotNull String[] allClassNames = allClassCache.getAllClassNames();
+        List<PsiClass> beanUtilClasses = new ArrayList<>();
+        for (String className : allClassNames) {
+            if (className.contains("BeanUtil") || className.contains("TypeUtil")) {
+                @NotNull PsiClass[] classesByName = allClassCache.getClassesByName(className, GlobalSearchScope.allScope(project));
+                beanUtilClasses.addAll(Arrays.stream(classesByName).toList());
+            }
+        }
+        // 筛选出使用这些BeanUtil调用目标类的结果集
+        Collection<PsiReference> filteredReference = new ArrayList<>();
+        for (PsiClass beanUtilClass : beanUtilClasses) {
+            Query<PsiReference> classReference = ReferencesSearch.search(beanUtilClass);
+            Collection<PsiReference> classReferenceAll = classReference.findAll();
+            for (PsiReference ref : classReferenceAll) {
+                if (ref instanceof PsiReferenceExpression) {
+                    PsiElement pre = (PsiReferenceExpression) ref;
+                    PsiMethodCallExpression pmce = null;
+                    while (true) {
+                        if (pre == null) {
+                            break;
+                        }
+                        if (pre instanceof PsiMethodCallExpression) {
+                            pmce = (PsiMethodCallExpression) pre;
+                            break;
+                        } else {
+                            pre = pre.getParent();
+                        }
+                    }
+                    if (pmce != null) {
+                        PsiExpression[] psiExpressions = pmce.getArgumentList().getExpressions();
+                        for (PsiExpression pe : psiExpressions) {
+                            if (pe.getType() instanceof PsiClassType pct) {
+                                PsiClass searchedClass = pct.resolve();
+                                if (searchedClass == psiClass) {
+                                    // 查询到了被BeanUtil调用的该类型
+                                    filteredReference.add(ref);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        // 收集结果集并显示
+        List<Usage> usages = new ArrayList<>();
+        for (PsiReference reference : filteredReference) {
+            UsageInfo usageInfo = new UsageInfo(reference);
+            Usage usage = new UsageInfo2UsageAdapter(usageInfo);
+            usages.add(usage);
+        }
+        UsageViewPresentation presentation = new UsageViewPresentation();
+        presentation.setTabText("Searched For BeanUtils");
+        UsageViewManager.getInstance(project).showUsages(UsageTarget.EMPTY_ARRAY, usages.toArray(new Usage[0]), presentation);
     }
 
     @Override
